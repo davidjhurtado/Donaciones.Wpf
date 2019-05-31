01881664e60e46e921698d97875aa09706a683ee
davidjhurtado <davidjhurtado@gmail.com>
2019-05-31 13:47:53 -0400

    Se agregó un Startup Bootraper para inyectar las dependencias al proyecto.

---
+++ Donaciones.WPF/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donaciones.WPF {
+    public class Startup {
+        public IContainer Bootstrap() {
+            ContainerBuilder containerbuilder = new ContainerBuilder();
+            containerbuilder.RegisterType<DonacionesDBEntities>().AsSelf();
+            containerbuilder.RegisterType<BeneficiariosRepository>().As<IBeneficiariosRepository>();
+            containerbuilder.RegisterType<ProductosRepository>().As<IProductosRepository>();
+            containerbuilder.RegisterType<BeneficiariosViewModel>().AsSelf();
+            containerbuilder.RegisterType<ProductosViewModel>().AsSelf();
+            containerbuilder.RegisterType<ProductosView>().AsSelf();
+            containerbuilder.RegisterType<MainWindow>().AsSelf();
+            return containerbuilder.Build();
+        }
+    }
+}
