104981d2eadcfe3f18d0303d7e54b1c2a0257c58
davidjhurtado <davidjhurtado@gmail.com>
2019-06-03 15:45:34 -0400

    Nuevo ViewModel para el Manejo de Ordenes

---
+++ Donaciones.WPF/ViewModel/OrdenesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donaciones.WPF {
+    public interface IOrdenesViewModel { }
+    public class OrdenesViewModel :BaseViewModelComputed, IOrdenesViewModel {
+
+        private readonly IOrdenesRepository ordenesRepository;
+        private ObservableCollection<OrdenesDetalle> obcOrdenesDetalle;
+
+        public OrdenesViewModel(IOrdenesRepository ordenesRepository) {
+            this.ordenesRepository = ordenesRepository;
+        }
+    }
+}
