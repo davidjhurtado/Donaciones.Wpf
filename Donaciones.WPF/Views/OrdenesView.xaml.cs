b482c5c8c781a59c06a333449ec99cc9dbac6a99
davidjhurtado <davidjhurtado@gmail.com>
2019-06-04 10:36:16 -0400

    Nuevo View de las Ordenes

---
+++ Donaciones.WPF/Views/OrdenesView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Donaciones.WPF {
+    /// <summary>
+    /// Lógica de interacción para OrdenesView.xaml
+    /// </summary>
+    public partial class OrdenesView :Window {
+        private readonly IOrdenesViewModel ordenesViewModel;
+
+        public OrdenesView(IOrdenesViewModel ordenesViewModel) {
+            InitializeComponent();
+            this.ordenesViewModel = ordenesViewModel;
+            DataContext = this.ordenesViewModel;
+        }
+    }
+}
