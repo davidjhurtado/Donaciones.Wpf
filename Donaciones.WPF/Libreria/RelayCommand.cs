8959b855bba46002a1b2398e71d843888ef6c0f1
davidjhurtado <davidjhurtado@gmail.com>
2019-06-06 08:54:46 -0400

    Se agregar una clase para el manejo de Commands con parametros

--- Donaciones.WPF/Libreria/RelayCommand.cs
+++ Donaciones.WPF/Libreria/RelayCommand.cs
@@ -32,4 +32,32 @@ namespace Donaciones.WPF {
             CommandManager.InvalidateRequerySuggested();
         }
     }
+
+    public class RelayCommand<T> :ICommand where T:class {
+        private Action<T> ExecuteAction { get; set; }
+        private Func<bool> CanExecuteAction { get; set; }
+
+        public RelayCommand(Action<T> execute,Func<bool> canExecute = null) {
+            ExecuteAction = execute;
+            CanExecuteAction = canExecute;
+        }
+
+        public bool CanExecute(object parameter) {
+            return CanExecuteAction == null || CanExecuteAction();
+        }
+
+        public void Execute(object parameter) {
+            var param = parameter as T;
+            ExecuteAction(param);
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Refresh() {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
 }
