3b1bd7de941994282128e620b4746b1aca8b33e8
davidjhurtado <davidjhurtado@gmail.com>
2019-06-06 08:56:08 -0400

    Manejo de notificación a traves de interaccion creada en la lista.

--- Donaciones.WPF/ViewModel/ListProductosViewModel.cs
+++ Donaciones.WPF/ViewModel/ListProductosViewModel.cs
@@ -10,13 +10,36 @@ namespace Donaciones.WPF {
     public interface IListProductosViewModel { }
     public class ListProductosViewModel :BaseViewModelComputed,IListProductosViewModel {
         private readonly IProductosRepository productosRepository;
-        public IEnumerable<Productos> lstProductos { get; set; }
-        public ICommand SeleccionarElementoCommand;
+        public ObservableCollection<Productos> lstProductos { get; set; }
+        public ICommand SeleccionarElementoCommand { get; set; }
         public ListProductosViewModel( IProductosRepository productosRepository) {
             this.productosRepository = productosRepository;
-            lstProductos = this.productosRepository.GetProductos();
+            lstProductos = new ObservableCollection<Productos>();
+            LoadProductos();
             RaisePropertyChanged(nameof(lstProductos));
+            this.SeleccionarElementoCommand = new RelayCommand<Productos>(SeleccionarElementoCommand_Executed,SeleccionarElementoCommand_CanExecute);
         }
 
+        private void LoadProductos() {
+            lstProductos.Clear();
+            IEnumerable<Productos> productos = productosRepository.GetProductos();
+            foreach (var producto in productos) {
+                lstProductos.Add(producto);
+            }
+        }
+
+        private bool SeleccionarElementoCommand_CanExecute() {
+            return true;
+        }
+
+        private void SeleccionarElementoCommand_Executed(Productos producto) {
+            System.Windows.MessageBox.Show("DoubleClick " + producto.NombreProducto);
+        }
+
+        
+
+
+
+        
     }
 }
